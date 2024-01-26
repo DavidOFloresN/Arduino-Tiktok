125c5f232fd00a34a0766ab401d4ecc6b79d65f2
DavidOFloresN <157840130+DavidOFloresN@users.noreply.github.com>
2024-01-26 16:10:10 -0700

    Update Encender_ApagarLED.ino

--- Encender y Apagar LED/Encender_ApagarLED.ino
+++ Encender y Apagar LED/Encender_ApagarLED.ino
@@ -2,12 +2,10 @@ int ledPin = 8
 int pause = 1000
 
 void setup() {
-  // put your setup code here, to run once:
 pinMode(ledPin,OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {  
 digitalWrite(ledPin,HIGH);
 delay(pause);
 digitalWrite(LedPin,LOW);
